df76fb5c7dde0ab57d1288792ce197eaf03056fc
AndreaBaraldi99 <andrea.baraldi99@hotmail.it>
2024-01-03 18:46:23 +0100

    Location updates fixes

--- app/src/main/java/it/lanos/eventbuddy/data/source/models/Location.java
+++ app/src/main/java/it/lanos/eventbuddy/data/source/models/Location.java
@@ -1,16 +1,16 @@
 package it.lanos.eventbuddy.data.source.models;
 
 public class Location {
+
     public String userId;
 
     public String eventId;
     public double latitude;
     public double longitude;
 
-    public Location(double latitude, double longitude, String userId, String eventId) {
+    public Location(double latitude, double longitude, String eventId) {
         this.latitude = latitude;
         this.longitude = longitude;
-        this.userId = userId;
         this.eventId = eventId;
     }
 
