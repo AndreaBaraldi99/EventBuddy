24d8f413e051cfc72ea05f6ca956140aa3981dc0
Simone <greymonar@hotmail.com>
2024-01-12 00:57:16 +0100

    Background users tracking

---
+++ app/src/main/java/it/lanos/eventbuddy/util/LocationClient.kt
@@ -0,0 +1,10 @@
+package it.lanos.eventbuddy.util
+
+import android.location.Location
+import kotlinx.coroutines.flow.Flow
+
+interface LocationClient {
+    fun getLocationUpdates(interval: Long): Flow<Location>
+
+    class LocationException(message: String): Exception()
+}
